b87d2fe66bf9d5b127c76fa0b7054c36284ad580
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2022-06-15 22:20:37 +0800

    Add start packager shell script

--- src/xcodeAppClip/addBuildPhases.ts
+++ src/xcodeAppClip/addBuildPhases.ts
@@ -11,20 +11,24 @@ export default function addBuildPhases(
 ) {
   const buildPath = quoted("$(CONTENTS_FOLDER_PATH)/AppClips");
 
-  // Add shell script build phase
-  const { uuid: shellScriptBuildPhaseUuid, buildPhase: shellScriptBuildPhase } =
-    proj.addBuildPhase(
-      [],
-      "PBXShellScriptBuildPhase",
-      "Bundle React Native code and images",
-      targetUuid,
-      {
-        shellPath: "/bin/sh",
-        shellScript: `export NODE_BINARY=node\\n\\n# The project root by default is one level up from the ios directory\\nexport PROJECT_ROOT=\"$PROJECT_DIR\"/..\\n\\n\`node --print \"require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/react-native-xcode.sh'\"\`\\n`,
-      },
-      buildPath
-    );
-  console.log(`Added PBXShellScriptBuildPhase ${shellScriptBuildPhaseUuid}`);
+  // Add shell script build phase "Start Packager"
+  const {
+    uuid: startPackagerShellScriptBuildPhaseUuid,
+    buildPhase: startPackagerShellScriptBuildPhase,
+  } = proj.addBuildPhase(
+    [],
+    "PBXShellScriptBuildPhase",
+    "Start Packager",
+    targetUuid,
+    {
+      shellPath: "/bin/sh",
+      shellScript: `export RCT_METRO_PORT=\"\${RCT_METRO_PORT:=8081}\"\\necho \"export RCT_METRO_PORT=\${RCT_METRO_PORT}\" > \`node --print \"require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/.packager.env'\"\`\\nif [ -z \"\${RCT_NO_LAUNCH_PACKAGER+xxx}\" ] ; then\\n  if nc -w 5 -z localhost \${RCT_METRO_PORT} ; then\\n    if ! curl -s \"http://localhost:\${RCT_METRO_PORT}/status\" | grep -q \"packager-status:running\" ; then\\n      echo \"Port \${RCT_METRO_PORT} already in use, packager is either not running or not running correctly\"\\n      exit 2\\n    fi\\n  else\\n    open \`node --print \"require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/launchPackager.command'\"\` || echo \"Can't start packager automatically\"\\n  fi\\nfi\\n`,
+    },
+    buildPath
+  );
+  console.log(
+    `Added PBXShellScriptBuildPhase ${startPackagerShellScriptBuildPhaseUuid}`
+  );
 
   // Copy files build phase
   const { uuid: copyFilesBuildPhaseUuid, buildPhase: copyFilesBuildPhase } =
@@ -61,4 +65,23 @@ export default function addBuildPhases(
       buildPath
     );
   console.log(`Added PBXSourcesBuildPhase ${sourcesBuildPhaseUuid}`);
+
+  // Add shell script build phase
+  const {
+    uuid: bundleShellScriptBuildPhaseUuid,
+    buildPhase: bundleShellScriptBuildPhase,
+  } = proj.addBuildPhase(
+    [],
+    "PBXShellScriptBuildPhase",
+    "Bundle React Native code and images",
+    targetUuid,
+    {
+      shellPath: "/bin/sh",
+      shellScript: `export NODE_BINARY=node\\n\\n# The project root by default is one level up from the ios directory\\nexport PROJECT_ROOT=\"$PROJECT_DIR\"/..\\n\\n\`node --print \"require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/react-native-xcode.sh'\"\`\\n`,
+    },
+    buildPath
+  );
+  console.log(
+    `Added PBXShellScriptBuildPhase ${bundleShellScriptBuildPhaseUuid}`
+  );
 }

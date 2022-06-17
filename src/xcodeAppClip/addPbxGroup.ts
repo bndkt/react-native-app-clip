f1383828834284c682ec784e7d688aba65790230
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2022-06-18 01:20:20 +0800

    Add missing files

--- src/xcodeAppClip/addPbxGroup.ts
+++ src/xcodeAppClip/addPbxGroup.ts
@@ -1,9 +1,46 @@
 import { XcodeProject } from "@expo/config-plugins";
+import * as fs from "fs";
+import * as path from "path";
 
 export default function addPbxGroup(
   proj: XcodeProject,
-  { appName, appClipName }: { appName: string; appClipName: string }
+  {
+    appName,
+    appClipName,
+    platformProjectRoot,
+  }: { appName: string; appClipName: string; platformProjectRoot: string }
 ) {
+  // App Clip folder
+  const appClipPath = path.join(platformProjectRoot, appClipName);
+
+  // Copy Expo.plist
+  const supportingPath = path.join(appClipPath, "Supporting");
+  const expoPlistSource = path.join(
+    platformProjectRoot,
+    appName,
+    "Supporting",
+    "Expo.plist"
+  );
+  fs.mkdirSync(supportingPath);
+  copyFileSync(expoPlistSource, supportingPath);
+
+  // Copy SplashScreen.storyboard
+  const splashScreenStoryboardSource = path.join(
+    platformProjectRoot,
+    appName,
+    "SplashScreen.storyboard"
+  );
+  copyFileSync(splashScreenStoryboardSource, appClipPath);
+
+  // Copy Images.xcassets
+  const imagesXcassetsSource = path.join(
+    platformProjectRoot,
+    appName,
+    "Images.xcassets"
+  );
+
+  copyFolderRecursiveSync(imagesXcassetsSource, appClipPath);
+
   // Add PBX group
   const { uuid: pbxGroupUuid, pbxGroup } = proj.addPbxGroup(
     [
@@ -11,13 +48,15 @@ export default function addPbxGroup(
       "AppDelegate.mm",
       "main.m",
       "Info.plist",
-      `../${appName}/Assets.xcassets`,
+      "Images.xcassets",
+      "SplashScreen.storyboard",
+      "Supporting/Expo.plist",
       `${appClipName}.entitlements`,
     ],
     appClipName,
     appClipName
   );
-  console.log(`Added PBXGroup ${pbxGroupUuid}`);
+  console.log(`Added PBXGroup ${pbxGroupUuid}`, pbxGroup);
 
   /* let pbxGroupUuid: string | undefined = undefined;
 
@@ -39,6 +78,40 @@ export default function addPbxGroup(
         console.log(
           `Added PBXGroup ${pbxGroupUuid} root PBXGroup group ${key}`
         );
+      }
+    });
+  }
+}
+
+function copyFileSync(source: any, target: any) {
+  console.log("copyFileSync", { source, target });
+  let targetFile = target;
+
+  if (fs.existsSync(target)) {
+    console.log("exists", target);
+    if (fs.lstatSync(target).isDirectory()) {
+      console.log("isDir", target);
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source: any, target: any) {
+  const targetPath = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetPath)) {
+    fs.mkdirSync(targetPath);
+  }
+
+  if (fs.lstatSync(source).isDirectory()) {
+    const files = fs.readdirSync(source);
+    files.forEach((file) => {
+      const currentPath = path.join(source, file);
+      if (fs.lstatSync(currentPath).isDirectory()) {
+        copyFolderRecursiveSync(currentPath, targetPath);
+      } else {
+        copyFileSync(currentPath, targetPath);
       }
     });
   }

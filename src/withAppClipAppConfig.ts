9eef35c379777d64762a1e3b67e84daadaa17ea0
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2022-06-25 09:13:34 +0800

    Improve app config modification

--- src/withAppClipAppConfig.ts
+++ src/withAppClipAppConfig.ts
@@ -1,48 +1,51 @@
-import { ConfigPlugin, withDangerousMod } from "@expo/config-plugins";
+import { ConfigPlugin } from "@expo/config-plugins";
 
 import { getAppClipBundleIdentifier, getAppClipFolder } from "./withIosAppClip";
 
 export const withAppClipAppConfig: ConfigPlugin = (config) => {
-  return withDangerousMod(config, [
-    "ios",
-    async (config) => {
-      const appBundleIdentifier = config.ios!.bundleIdentifier!;
-      const appClipName = getAppClipFolder(config.modRequest.projectName!);
-      const appClipBundleIdentifier = getAppClipBundleIdentifier(
-        config.modRequest.projectName!
-      );
+  const appBundleIdentifier = config.ios!.bundleIdentifier!;
+  const appClipName = getAppClipFolder(config.name);
+  const appClipBundleIdentifier = getAppClipBundleIdentifier(
+    config.ios!.bundleIdentifier!
+  );
 
-      config.extra = {
-        ...config.extra,
-        eas: {
-          ...config.extra?.eas,
-          build: {
-            ...config.extra?.eas?.build,
-            experimental: {
-              ...config.extra?.eas?.build?.experimental,
-              ios: {
-                ...config.extra?.eas?.build?.experimental?.ios,
-                appExtensions: [
-                  ...(config.extra?.eas?.build?.experimental?.ios
-                    ?.appExtensions ?? []),
-                  {
-                    targetName: appClipName,
-                    bundleIdentifier: `$(AppIdentifierPrefix)${appClipBundleIdentifier}`,
-                    entitlements: {
-                      "com.apple.developer.parent-application-identifiers": [
-                        `$(AppIdentifierPrefix)${appBundleIdentifier}`,
-                      ],
-                      "com.apple.developer.on-demand-install-capable": true,
-                    },
+  let appClipConfigExists = false;
+  config.extra?.eas?.build?.experimental?.ios?.appExtensions?.forEach(
+    (ext: any) => {
+      ext.targetName === appClipName && (appClipConfigExists = true);
+    }
+  );
+
+  !appClipConfigExists &&
+    (config.extra = {
+      ...config.extra,
+      eas: {
+        ...config.extra?.eas,
+        build: {
+          ...config.extra?.eas?.build,
+          experimental: {
+            ...config.extra?.eas?.build?.experimental,
+            ios: {
+              ...config.extra?.eas?.build?.experimental?.ios,
+              appExtensions: [
+                ...(config.extra?.eas?.build?.experimental?.ios
+                  ?.appExtensions ?? []),
+                {
+                  targetName: appClipName,
+                  bundleIdentifier: `$(AppIdentifierPrefix)${appClipBundleIdentifier}`,
+                  entitlements: {
+                    "com.apple.developer.parent-application-identifiers": [
+                      `$(AppIdentifierPrefix)${appBundleIdentifier}`,
+                    ],
+                    "com.apple.developer.on-demand-install-capable": true,
                   },
-                ],
-              },
+                },
+              ],
             },
           },
         },
-      };
+      },
+    });
 
-      return config;
-    },
-  ]);
+  return config;
 };

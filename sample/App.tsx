65feb9d06b999afa53e80ef2a0e582f93cc62e9e
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2022-10-10 12:16:07 +0800

    Add App Clip indicator

--- sample/App.tsx
+++ sample/App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function App({ isClip }: { isClip: boolean }) {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ marginTop: 10, fontWeight: "bold" }}>
+        {isClip ? "App Clip" : "Not App Clip"}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

8beb64a564c5075f807885162d2ede806a10fd11
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-08-09 21:18:05 +0800

    Add plugin

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-import * as ReactNativeAppClip from 'react-native-app-clip';
+import * as ReactNativeAppClip from "react-native-app-clip";
 
 export default function App() {
   return (
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

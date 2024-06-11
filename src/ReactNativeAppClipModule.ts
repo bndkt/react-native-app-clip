252c88b863e81a42d4ce8324e69ea6e727935ca3
AlexandreFauchard <alexandre.fauchard@toolpad.fr>
2024-06-11 11:29:21 +0200

    Remove errors when missing native module (web, android, legacy ios build)

--- src/ReactNativeAppClipModule.ts
+++ src/ReactNativeAppClipModule.ts
@@ -1,5 +1,30 @@
 import { requireNativeModule } from "expo-modules-core";
+import { Platform } from "react-native";
+
+export type ReactNativeAppClipModule = {
+  getBundleIdentifier(): string;
+  getContainerURL(groupIdentifier: string): string;
+  getBundleIdentifier(): string;
+  displayOverlay(): void;
+  setSharedCredential(groupIdentifier: string, credential: string): void;
+  getSharedCredential(groupIdentifier: string): string;
+}
+
+const isIos = Platform.OS === 'ios';
+
+const getNativeModule = () => {
+  if(isIos){
+    try{
+      return requireNativeModule("ReactNativeAppClip") as ReactNativeAppClipModule;
+    }catch{
+      return undefined;
+    }
+  }
+  return undefined;
+}
 
 // It loads the native module object from the JSI or falls back to
 // the bridge module (from NativeModulesProxy) if the remote debugger is on.
-export default requireNativeModule("ReactNativeAppClip");
+const appClipModule = getNativeModule();
+
+export default appClipModule

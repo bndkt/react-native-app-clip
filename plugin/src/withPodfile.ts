156971b2ff830e1cbf3325e0b4e7cd0cd4c67e50
Nathan Ahn <nathancahn@gmail.com>
2024-11-14 15:18:23 -0500

    App clip podfile insertion fix

--- plugin/src/withPodfile.ts
+++ plugin/src/withPodfile.ts
@@ -20,41 +20,32 @@ export const withPodfile: ConfigPlugin<{
 
       const useExpoModules =
         excludedPackages && excludedPackages.length > 0
-          ? `exclude = ["${excludedPackages.join(`", "`)}"]
-      use_expo_modules!(exclude: exclude)`
+          ? `exclude = ["${excludedPackages.join(`", "`)}"]\n  use_expo_modules!(exclude: exclude)`
           : "use_expo_modules!";
 
       const appClipTarget = `
-        target '${targetName}' do
-          ${useExpoModules}
-          config = use_native_modules!
+target '${targetName}' do
+  ${useExpoModules}
+  config = use_native_modules!
 
-          use_frameworks! :linkage => podfile_properties['ios.useFrameworks'].to_sym if podfile_properties['ios.useFrameworks']
-          use_frameworks! :linkage => ENV['USE_FRAMEWORKS'].to_sym if ENV['USE_FRAMEWORKS']
+  use_frameworks! :linkage => podfile_properties['ios.useFrameworks'].to_sym if podfile_properties['ios.useFrameworks']
+  use_frameworks! :linkage => ENV['USE_FRAMEWORKS'].to_sym if ENV['USE_FRAMEWORKS']
 
-          use_react_native!(
-            :path => config[:reactNativePath],
-            :hermes_enabled => podfile_properties['expo.jsEngine'] == nil || podfile_properties['expo.jsEngine'] == 'hermes',
-            # An absolute path to your application root.
-            :app_path => "#{Pod::Config.instance.installation_root}/..",
-            :privacy_file_aggregation_enabled => podfile_properties['apple.privacyManifestAggregationEnabled'] != 'false',
-          )
-        end
-      `;
+  use_react_native!(
+    :path => config[:reactNativePath],
+    :hermes_enabled => podfile_properties['expo.jsEngine'] == nil || podfile_properties['expo.jsEngine'] == 'hermes',
+    # An absolute path to your application root.
+    :app_path => "#{Pod::Config.instance.installation_root}/..",
+    :privacy_file_aggregation_enabled => podfile_properties['apple.privacyManifestAggregationEnabled'] != 'false',
+  )
+end
+      `.trim();
 
-      /* podfileContent = podfileContent
-        .concat(`\n\n# >>> Inserted by react-native-app-clip`)
-        .concat(podfileInsert)
-        .concat(`\n\n# <<< Inserted by react-native-app-clip`); */
-
-      podfileContent = mergeContents({
-        tag: "react-native-app-clip-2",
-        src: podfileContent,
-        newSrc: appClipTarget,
-        anchor: "Pod::UI.warn e",
-        offset: 3,
-        comment: "#",
-      }).contents;
+      // It's generally safer to use `mergeContents` to insert new content into the Podfile, but this new target should always be inserted at the end of the file.
+      podfileContent = podfileContent
+        .concat(`\n\n# >>> Inserted by react-native-app-clip\n`)
+        .concat(appClipTarget)
+        .concat(`\n\n# <<< Inserted by react-native-app-clip\n`);
 
       fs.writeFileSync(podFilePath, podfileContent);
 

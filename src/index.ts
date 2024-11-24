8ca326526fde13546d375d0c2b7da509a86d6f62
Nathan Ahn <nathancahn@gmail.com>
2024-11-24 11:08:17 -0500

    App clip import fix

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
+import ReactNativeAppClipModule from "./ReactNativeAppClipModule/ReactNativeAppClipModule";
+import { ReactNativeAppClipModuleType } from "./ReactNativeAppClipModule/types";
 import getBundleIdentifier from "./getBundleIdentifier";
 import isClip from "./isClip";
-import ReactNativeAppClipModuleIos from "./ReactNativeAppClipModule/ReactNativeAppClipModule.ios";
-import { ReactNativeAppClipModuleType } from "./ReactNativeAppClipModule/types";
 
-export const getContainerURL: ReactNativeAppClipModuleType["getContainerURL"] =
-	(groupIdentifier) => {
-		return ReactNativeAppClipModuleIos.getContainerURL(groupIdentifier);
-	};
+export const getContainerURL: ReactNativeAppClipModuleType["getContainerURL"] = (
+	groupIdentifier,
+) => {
+	return ReactNativeAppClipModule.getContainerURL(groupIdentifier);
+};
 
-export const displayOverlay: ReactNativeAppClipModuleType["displayOverlay"] =
-	() => {
-		return ReactNativeAppClipModuleIos.displayOverlay();
-	};
+export const displayOverlay: ReactNativeAppClipModuleType["displayOverlay"] = () => {
+	return ReactNativeAppClipModule.displayOverlay();
+};
 
-export const setSharedCredential: ReactNativeAppClipModuleType["setSharedCredential"] =
-	(groupIdentifier, credential) => {
-		return ReactNativeAppClipModuleIos.setSharedCredential(
-			groupIdentifier,
-			credential,
-		);
-	};
+export const setSharedCredential: ReactNativeAppClipModuleType["setSharedCredential"] = (
+	groupIdentifier,
+	credential,
+) => {
+	return ReactNativeAppClipModule.setSharedCredential(groupIdentifier, credential);
+};
 
-export const getSharedCredential: ReactNativeAppClipModuleType["getSharedCredential"] =
-	(groupIdentifier) => {
-		return ReactNativeAppClipModuleIos.getSharedCredential(groupIdentifier);
-	};
+export const getSharedCredential: ReactNativeAppClipModuleType["getSharedCredential"] = (
+	groupIdentifier,
+) => {
+	return ReactNativeAppClipModule.getSharedCredential(groupIdentifier);
+};
 
 export { getBundleIdentifier, isClip };
